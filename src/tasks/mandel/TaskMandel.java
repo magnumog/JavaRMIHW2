1e0102c390188dcf7a0a18cbe3334c3a1762993a
Magnus Settemsli Mogstad <magnumog@stud.ntnu.no>
2015-04-20 14:30:12 -0700

    almost finished but need to go to class

--- src/tasks/mandel/TaskMandel.java
+++ src/tasks/mandel/TaskMandel.java
@@ -1,5 +1,30 @@
 package tasks.mandel;
 
-public class TaskMandel {
+import api.Task;
+
+public class TaskMandel implements Task<ResultValueMandelbrotSet> {
+	double lowerX;
+	double lowerY;
+	double edgeLength;
+	int numPixles;
+	int iterationLimit;
+	int blockRow;
+	int blockCol;
+	
+	public TaskMandel(double lowerX,double lowerY,double edgeLength, int numPixles, int iterationLimit, int blockRow, int blockCol) {
+		this.lowerX = lowerX;
+		this.lowerY = lowerY;
+		this.edgeLength = edgeLength;
+		this.numPixles = numPixles;
+		this.iterationLimit = iterationLimit;
+		this.blockRow = blockRow;
+		this.blockCol = blockCol;
+	}
+
+	@Override
+	public ResultValueMandelbrotSet call() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
