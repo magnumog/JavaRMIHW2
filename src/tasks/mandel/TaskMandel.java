e3d01aa8b468744310edbe7275515b2c86881d87
magnumog <magnumog@stud.ntnu.no>
2015-04-20 19:22:56 -0700

    mandeltask should be finished now

--- src/tasks/mandel/TaskMandel.java
+++ src/tasks/mandel/TaskMandel.java
@@ -23,8 +23,28 @@ public class TaskMandel implements Task<ResultValueMandelbrotSet> {
 
 	@Override
 	public ResultValueMandelbrotSet call() {
-		// TODO Auto-generated method stub
-		return null;
+		Integer[][] counts = new Integer[numPixles][numPixles];
+		double delta = edgeLength/numPixles;
+		for(int i=0;i<numPixles;i++) {
+			for(int j=0;j<numPixles;j++) {
+				counts[i][j] = getIterationCount(i,j,delta);
+			}
+		}
+		return new ResultValueMandelbrotset(counts,blockRow,blockCol);
 	}
+
+	private int getIterationCount(int i, int j, double delta) {
+		double x0 = lowerX+i*delta;
+		double y0 = lowerY+j*delta;
+		int iteration = 0;
+		for(double x =x0,y=y0; x*x+y*y<=4.0 && iteration<iterationLimit;iteration++) {
+			double xtemp = x*x-y*y+x0;
+			y=2*x*y+y0;
+			x=xtemp;
+		}
+		return iteration;
+	}
+	
+	
 
 }
